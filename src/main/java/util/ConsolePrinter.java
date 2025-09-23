a74f605b096155915096c2539dc9d5a8f22fcb83
Alexander <diosespectro@gmail.com>
2025-09-23 11:29:44 +0500

    Реализован метод printArray (ConsolePrinter)

--- src/main/java/util/ConsolePrinter.java
+++ src/main/java/util/ConsolePrinter.java
@@ -1,8 +1,22 @@
 package util;
 
+import java.util.Arrays;
+
 public class ConsolePrinter {
-
     public static void printArray(String message, double[] array) {
+        String value = "";
 
+        // Проверяем, что массив не null
+        if (array == null) {
+            value = "Массив равен null";
+        }
+        else
+        // Проверяем, что массив не пустой
+        if (array.length == 0) {
+            value = "Массив пуст";
+        }
+        else value = Arrays.toString(array);
+
+        System.out.println(message + " " + value);
     }
 }
